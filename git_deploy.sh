e9a1513b12d330a90fbd22d01b4ddb3a3f44751b
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-26 11:00:02 -0700

    attempt fix deploy script #9

--- git_deploy.sh
+++ git_deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 {
-  git pull
+  git fetch --unshallow
 } > /dev/null 2>&1
 
 git config --global user.email 'myrmecocystus@gmail.com' && git config --global user.name 'Scott Chamberlain'
@@ -10,5 +10,5 @@ echo $(date) > date.txt
 git add date.txt
 git commit -m 'built and deployed from circle [ci skip]'
 {
-  git push https://sckott:$GH_TOKEN@github.com/sckott/msrgbif gh-pages
+  git push https://sckott:$GH_TOKEN@github.com/sckott/gbifms gh-pages
 } &> /dev/null 2>&1
